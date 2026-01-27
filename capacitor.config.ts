469fcda0bc4b2703cb035d25a21b943332cad0a6
Allyson <allysonmathews52@gmail.com>
2026-01-27 01:25:12 -0300

    Adicionando projeto Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'bilu-shape',
+  webDir: 'dist'
+};
+
+export default config;
